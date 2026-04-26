e4774c010fb9da431aa269317d7579b85a275fc2
Edu <edu8987675@gmail.com>
2026-04-26 19:11:55 +0200

    Completado el los 3 Code

--- Tutorial/src/main/java/com/ccsw/Tutorial/category/CategoryService.java
+++ Tutorial/src/main/java/com/ccsw/Tutorial/category/CategoryService.java
@@ -12,6 +12,14 @@ import java.util.List;
 public interface CategoryService {
 
     /**
+     * Recupera una {@link Category} a partir de su ID
+     *
+     * @param id PK de la entidad
+     * @return {@link Category}
+     */
+    Category get(Long id);
+
+    /**
      * Método para recuperar todas las {@link Category}
      *
      * @return {@link List} de {@link Category}
@@ -32,4 +40,5 @@ public interface CategoryService {
      * @param id PK de la entidad
      */
     void delete(Long id) throws Exception;
+
 }
---
+++ Tutorial/src/main/java/com/ccsw/Tutorial/game/GameRepository.java
@@ -0,0 +1,21 @@
+package com.ccsw.Tutorial.game;
+
+import com.ccsw.Tutorial.game.model.Game;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+/**
+ * @author ccsw
+ *
+ */
+public interface GameRepository extends CrudRepository<Game, Long>, JpaSpecificationExecutor<Game> {
+
+    @Override
+    @EntityGraph(attributePaths = { "category", "author" })
+    List<Game> findAll(Specification<Game> spec);
+
+}
